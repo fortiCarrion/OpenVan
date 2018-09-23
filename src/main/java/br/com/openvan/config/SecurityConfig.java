aaa5dd8445fd86e2cdc9b842e5202c1d33a8b30d
fortiCarrion <jaircarrion@hotmail.com>
2018-09-23 11:12:35 -0300

    Vercionamento Security

---
+++ src/main/java/br/com/openvan/config/SecurityConfig.java
@@ -0,0 +1,38 @@
+package br.com.openvan.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+	
+	private static final String[] PUBLIC_MATHERS = {
+			"/h2-console/**",
+			"/colegios/**",
+			"/veiculos/**",
+			"/alunos/**"
+	};
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.cors();
+		http.authorizeRequests()
+			.antMatchers(PUBLIC_MATHERS).permitAll()
+			.anyRequest().authenticated();
+	}
+	
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+		return source;
+	}
+
+}
